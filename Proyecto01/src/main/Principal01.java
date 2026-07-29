73b36c540cdf7bd6ebf84f322d6c72b5328e20b8
SoyAdV <79457088+SoyAdV@users.noreply.github.com>
2026-07-29 16:50:35 -0600

    se agregan proyectos a main

---
+++ Proyecto01/src/main/Principal01.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Principal01 {
+
+	public static void main(String[] args) {
+		
+		Principal02 p2 = new Principal02();
+		Principal03 p3 = new Principal03();
+		
+		System.out.println("Programa Principal01");
+		System.out.println(p2.toString());
+		System.out.println(p3.toString());
+	}		
+}
---
+++ Proyecto03/src/main/Principal03.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Principal03 {
+	@Override
+	public String toString() {
+		return "\n"+super.toString()+"\nEste es el proyecto1";
+	}
+}
